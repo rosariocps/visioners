8bec78c88ce411bdf18b0b4ccdaba5b7fc67a59e
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-06-15 17:03:50 -0500

    Agregado de comentarios en el constructor de clase Vertice

--- graph/Vertice.java
+++ graph/Vertice.java
@@ -4,13 +4,14 @@ import linkedlist.ListaEnlazada;
 
 public class Vertice<E> {
 
-    // Atributos
+    // ATRIBUTOS
     private E dato; //Representa la información del vértice
     protected ListaEnlazada<Arista<E>> listaAdyacencia; // Contiene todas las aristas salientes de este vértice 
 
-    public Vertice(E dato) { //contructor
-        this.dato = dato;
-        listaAdyacencia = new ListaEnlazada<>();
+    // CONSTRUCTOR
+    public Vertice(E dato) {
+        this.dato = dato; // Se asigna el valor recibido al atributo 'dato'
+        listaAdyacencia = new ListaEnlazada<>(); // Se inicializa la lista de adyacencia vacía
     }
     
 }
