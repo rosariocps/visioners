db34491fdcea48a088cf75bf0874af875ef72ffc
ucsmfernanda <linaresfernanda.yj@gmail.com>
2025-06-15 18:07:24 -0500

    Agregado del método getListaAdyacencia en la clase vertice.

--- graph/Vertice.java
+++ graph/Vertice.java
@@ -4,17 +4,23 @@ import linkedlist.ListaEnlazada;
 
 public class Vertice<E> {
 
-    // Atributos
+    // ATRIBUTOS
     private E dato; //Representa la información del vértice
     protected ListaEnlazada<Arista<E>> listaAdyacencia; // Contiene todas las aristas salientes de este vértice 
 
-    public Vertice(E dato) { //contructor
-        this.dato = dato;
-        listaAdyacencia = new ListaEnlazada<>();
+    //CONSTRUCTOR
+    public Vertice(E dato) { 
+        this.dato = dato; // Se asigna el valor recibido al atributo 'dato'
+        listaAdyacencia = new ListaEnlazada<>(); //Se inicializa a lista de adyacencia vacía
     }
     
     //Metodo para obtener el dato almacenado en el vertice
     public E getDato() {
-    	return dato;
+    	return dato; // Retorna el valor almacenado en el atributo 'dato'
+    }
+    
+    // Método para obtener la lista de adyacencia (las aristas conectadas a este vértice)
+    public ListaEnlazada<Arista<E>> getListaAdyacencia() {
+        return listaAdyacencia; // Retorna la lista de aristas que representan las conexiones de este vértice
     }
 }
