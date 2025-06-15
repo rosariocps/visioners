0ce61b4d7b5ee2c1bb5daabec337a419c1a899ea
ucsmfernanda <linaresfernanda.yj@gmail.com>
2025-06-15 17:40:17 -0500

    Agregado del metodo getDato de la clase vertice.

--- graph/Vertice.java
+++ graph/Vertice.java
@@ -4,14 +4,17 @@ import linkedlist.ListaEnlazada;
 
 public class Vertice<E> {
 
-    // ATRIBUTOS
+    // Atributos
     private E dato; //Representa la información del vértice
     protected ListaEnlazada<Arista<E>> listaAdyacencia; // Contiene todas las aristas salientes de este vértice 
 
-    // CONSTRUCTOR
-    public Vertice(E dato) {
-        this.dato = dato; // Se asigna el valor recibido al atributo 'dato'
-        listaAdyacencia = new ListaEnlazada<>(); // Se inicializa la lista de adyacencia vacía
+    public Vertice(E dato) { //contructor
+        this.dato = dato;
+        listaAdyacencia = new ListaEnlazada<>();
     }
     
+    //Metodo para obtener el dato almacenado en el vertice
+    public E getDato() {
+    	return dato;
+    }
 }
