b5dbebae04bb789b270e748af6f981cede1258f6
ucsmfernanda <linaresfernanda.yj@gmail.com>
2025-06-15 18:24:28 -0500

    Agregado del método ListaEnlazada de la clase vertice, con comentarios

--- graph/Vertice.java
+++ graph/Vertice.java
@@ -19,7 +19,7 @@ public class Vertice<E> {
     	return dato; // Retorna el valor almacenado en el atributo 'dato'
     }
     
-    // Método para obtener la lista de adyacencia (las aristas conectadas a este vértice)
+    // Método para obtener la lista de adyacencia (las aristas conectadas a este vértice) 
     public ListaEnlazada<Arista<E>> getListaAdyacencia() {
         return listaAdyacencia; // Retorna la lista de aristas que representan las conexiones de este vértice
     }
