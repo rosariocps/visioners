d08b5bfb7f7c8474e153a56765e5e1d92c1c7f76
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-06-15 13:54:13 -0500

    Creacion de clase GrafoDirigido

---
+++ graph/GrafoDirigido.java
@@ -0,0 +1,70 @@
+package graph;
+
+//import linkedlist.ListaEnlazada;
+//import linkedlist.Nodo;
+
+public class GrafoDirigido<E> {
+
+    // Lista de todos los vértices (ubicaciones del almacén)
+    //protected ListaEnlazada<Vertice<E>> listaVertices;
+
+    // Constructor
+    public GrafoDirigido() {
+        //listaVertices = new ListaEnlazada<>();
+    }
+
+    // ---------------- MÉTODOS PARA VÉRTICES ----------------
+
+    // INSERTAR UN NUEVO VERTICE
+    public void insertarVertice(E dato) {
+        
+    }
+
+    // ELIMINAR UN VÉRTICE Y SUS ARISTAS ASOCIADAS
+    public void eliminarVertice(E dato) {
+        
+    }
+
+    // MODIFICAR EL DATO DE UN VÉRTICE
+    public void modificarVertice(E datoAntiguo, E datoNuevo) {
+        
+    }
+
+    // BUSCAR UN VÉRTICE POR SU DATO
+    /*public Vertice<E> buscarVertice(E dato) {
+        
+        return null;
+    }*/
+
+    // ---------------- MÉTODOS PARA ARISTAS ----------------
+
+    // INSERTAR UNA ARISTA DIRIGIDA CON PESO
+    public void insertarArista(E origen, E destino, int peso) {
+        
+    }
+
+    // ELIMINAR UNA ARISTA DIRIGIDA
+    public void eliminarArista(E origen, E destino) {
+        
+    }
+
+    // MODIFICAR EL PESO DE UNA ARISTA EXISTENTE
+    public void modificarPesoArista(E origen, E destino, int nuevoPeso) {
+        
+    }
+
+    // VERIFICAR SI EXISTE UNA ARISTA ENTRE DOS VERTICES
+    public boolean existeArista(E origen, E destino) {
+        
+        return false;
+    }
+
+    // ---------------- UTILITARIO ----------------
+
+    // MOSTRAR EL GRAFO COMO TEXTO
+    @Override
+    public String toString() {
+        
+        return "";
+    }
+}
