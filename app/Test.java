c42b07ecd98da24ad534299fcc51bb828dcd0756
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-06-16 02:45:53 -0500

    Actualizacion de nombre de la clase Test

--- app/Test.java
+++ app/Test.java
@@ -2,7 +2,7 @@ package app;
 
 import graph.GrafoDirigido;
 
-public class test {
+public class Test {
     public static void main(String[] args) {
         GrafoDirigido<String> grafo = new GrafoDirigido<>();
 
