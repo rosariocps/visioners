1c0b94af3fb4440fd739521662121ec480353840
Janitza Tapia <janitzat4@gmail.com>
2025-06-22 13:40:43 -0500

    Producto.java

--- modelos/Producto.java
+++ modelos/Producto.java
@@ -1,5 +1,17 @@
 package modelos;
 
+// esta clase representa un producto en el almacen
 public class Producto {
-    
+    public String codigo; // codigo unico del producto
+    public String nombre; // nombre del producto
+
+    public Producto(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    // muestra los datos del producto
+    public void mostrarDatos() {
+        System.out.println("Producto: " + nombre + " | Código: " + codigo);
+    }
 }
