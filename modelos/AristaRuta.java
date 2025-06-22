a5ff12ba1a3ba995a0f773af77d0e380615fe381
Janitza Tapia <janitzat4@gmail.com>
2025-06-22 13:37:42 -0500

    AristaRuta.java

--- modelos/AristaRuta.java
+++ modelos/AristaRuta.java
@@ -1,5 +1,29 @@
 package modelos;
 
+// esta clase representa una ruta o camino entre dos ubicaciones
 public class AristaRuta {
-    
+    private int origen;   // nodo de donde sale
+    private int destino;  // nodo al que llega
+    private int peso;     // valor de la ruta (puede ser distancia o tiempo)
+
+    public AristaRuta(int origen, int destino, int peso) {
+        this.origen = origen;
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    // devuelve el nodo origen
+    public int obtenerOrigen() {
+        return origen;
+    }
+
+    // devuelve el nodo destino
+    public int obtenerDestino() {
+        return destino;
+    }
+
+    // devuelve el valor de la ruta
+    public int obtenerPeso() {
+        return peso;
+    }
 }
